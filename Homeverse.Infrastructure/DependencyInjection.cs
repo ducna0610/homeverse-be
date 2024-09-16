8ae55b6f5abcdd6ab1dd68c509d133dda89cd1aa
ducna0610 <ducna0610@gmail.com>
2024-09-16 22:44:07 +0700

    chore(infrastructure): add dependency injection

---
+++ Homeverse.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Homeverse.Domain.Interfaces;
+using Homeverse.Infrastructure.Data;
+using Homeverse.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Homeverse.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<ICityRepository, CityRepository>();
+        services.AddScoped<IContactRepository, ContactRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IPropertyRepository, PropertyRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+
+        return services;
+    }
+}
