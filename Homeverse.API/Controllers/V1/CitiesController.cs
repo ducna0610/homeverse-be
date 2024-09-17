f0a8d33b4bb8a8d9a1213e0908c445b2150cdaed
ducna0610 <ducna0610@gmail.com>
2024-09-17 22:19:00 +0700

    feat(city): add city controller

---
+++ Homeverse.API/Controllers/V1/CitiesController.cs
@@ -0,0 +1,136 @@
+﻿using Homeverse.Application.DTOs.Requests;
+using Homeverse.Application.DTOs.Responses;
+using Homeverse.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Asp.Versioning;
+
+namespace Homeverse.API.Controllers.V1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ILogger<CitiesController> _logger;
+        private readonly ICityService _cityService;
+
+        public CitiesController(ICityService cityService, ILogger<CitiesController> logger)
+        {
+            _logger = logger;
+            _cityService = cityService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CityResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var cities = await _cityService.GetCitiesAsync();
+
+                if (cities == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(cities);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"The method {nameof(CityService.GetCitiesAsync)} caused an exception", ex);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(CityResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var city = await _cityService.GetCityByIdAsync(id);
+
+                if (city == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(city);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"The method {nameof(CityService.GetCityByIdAsync)} caused an exception", ex);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(CityResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Add(CityRequest request)
+        {
+            try
+            {
+                var city = await _cityService.AddCityAsync(request);
+
+                return CreatedAtAction(nameof(GetById), new { id = city.Id }, city);
+            }
+            catch
+            (Exception ex)
+            {
+                _logger.LogError($"The method {nameof(CityService.AddCityAsync)} caused an exception", ex);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(CityResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update(int id, CityRequest request)
+        {
+            try
+            {
+                var city = await _cityService.UpdateCityAsync(id, request);
+
+                return Ok(city);
+            }
+            catch
+            (Exception ex)
+            {
+                _logger.LogError($"The method {nameof(CityService.UpdateCityAsync)} caused an exception", ex);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _cityService.DeleteCityAsync(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"The method {nameof(CityService.DeleteCityAsync)} caused an exception", ex);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
--- Homeverse.API/Program.cs
+++ Homeverse.API/Program.cs
@@ -1,28 +1,52 @@
 using Homeverse.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Homeverse.Application;
+using Homeverse.Infrastructure;
+using Asp.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddDbContext<HomeverseDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
 });
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+builder.Services
+    .AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        var descriptions = app.DescribeApiVersions();
+
+        foreach (var description in descriptions)
+        {
+            var url = $"/swagger/{description.GroupName}/swagger.json";
+            var name = description.GroupName.ToUpperInvariant();
+            options.SwaggerEndpoint(url, name);
+        }
+    });
 }
 
 app.UseAuthorization();
